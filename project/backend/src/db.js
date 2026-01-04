754f1aff9ad8ed5dab4b5265a981ac6ceb91c524
Alvin Deriaz <alvin@localhost.localdomain>
2026-01-04 13:08:34 +0300

    Fix pool init (remove query at import)

--- project/backend/src/db.js
+++ project/backend/src/db.js
@@ -2,17 +2,6 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 require("dotenv").config({ path: "../.env" });
 
-console.log("DB CONFIG =>", {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
-pool.query("SELECT DATABASE() AS db")
-    .then(([r]) => console.log("SELECT DATABASE() =>", r))
-    .catch(console.error);
-
-
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
